da2d6d06b4e074cefcc70cf531ffceec290f3421
Andrianina <ramiandrisoan5@gmail.com>
2025-01-18 21:30:50 +0300

    feat : reset_password (email doesn't work)

--- Cybexus/Cybexus/settings.py
+++ Cybexus/Cybexus/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#New Andrianina
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Exemple pour Gmail
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ramiandrisoan5@gmail.com'
+EMAIL_HOST_PASSWORD = 'Cybexus'
+DEFAULT_FROM_EMAIL = 'ramiandrisoan5@gmail.com'
+
--- Cybexus/authentification/urls.py
+++ Cybexus/authentification/urls.py
@@ -2,10 +2,16 @@
 from django.urls import path
 from django.shortcuts import redirect
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', lambda request: redirect('sign_in')),  # Redirige vers 'sing_in' par défaut
     path('sign_in/', views.sign_in , name = 'sign_in'), 
     path('login/', views.user_login, name = 'login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authentification/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='authentification/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentification/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='authentification/password_reset_complete.html'), name='password_reset_complete')
 
 ]
